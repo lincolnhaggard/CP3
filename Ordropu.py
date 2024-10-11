8cd54f8235bf4167583944ec77381ea8ff47163b
lincolnhaggard <lincoln.haggard@ucas-edu.net>
2024-10-11 21:38:06 +0000

    Fixed checker that there are items in the ordor and other things

--- Ordropu.py
+++ Ordropu.py
@@ -1,7 +1,7 @@
 menu={
     "Drink":{"Coke":10.99,"Root beer without the root":5,"Bebsi":4.72},
     "Appetizer":{"Green":255,"Yellow":23.23,"Orange":8.00},
-    "Main Course":{"Chicken salard":15.23,"Bime prib":32.4,"Road":0.25},
+    "Main course":{"Chicken salard":15.23,"Bime prib":32.4,"Road":0.25},
     "Side":{"Apple sus":5.6,"Spoon":3.45,"Laundry deterunt":7.5},
     "Second side":{"Apple sus":5.6,"Spoon":3.45,"Laundry deterunt":7.5},
     "Dessert":{"Sand":0.01,"Crab":14.5,"Scorpion":29.1}
@@ -41,7 +41,7 @@ class Ordor:
                     else:
                         self.items.append("")
         else:
-            self.items=Ordor.specials(self)
+            self.items=Ordor.specials()
             self.special=True
     def __str__(self):
         toret=""
@@ -51,11 +51,12 @@ class Ordor:
         for x,i in enumerate(self.items):
             if i!="":
                 toret+=f"{menukeys[x]}: {i}\n"
-        toret+=f"Total:{Ordor.price(self.items,False)}\n"
+        toret+=f"Total:{Ordor.price(self.items,self.special)}\n"
+        if self.special:
+            toret+="Special\n"
         return toret
     
     def changeitem(self,item):
-        print(item)
         for x,i in enumerate(menu.keys()):
             
             if item==i:
@@ -75,14 +76,14 @@ class Ordor:
                     self.items[x]=inp
                 break
         self.special=False
-    
-    @classmethod
     def checkordor(self):
-        self.items.remove("")
-        if len(self.items)>0:
-            return True
-        else:
-            return False
+        toret=False
+        for i in self.items:
+            if i!="":
+                toret=True
+        return toret
+    @classmethod
+    
     def specials(self):
         print("\nGreen Coke, Double spoon, prime bib\n")
         inp=input("What special would you like: \n").lower()
@@ -107,10 +108,13 @@ class Ordor:
             total*=0.9
         total=round(total,2)
         return format(total,".2f")
+    @staticmethod
     def viewordorlist(ordors):
         toret="\n\n-----\n"
         for x,i in enumerate(ordors):
             toret+=f"\nOrdor #{x+1}\n"+str(i)+"\n-----\n"
+        if len(ordors)<1:
+            toret="No ordors"
         return toret
 """
 o=Ordor()
@@ -126,9 +130,11 @@ while inp=="yes":
         ordors.append(Ordor(True))
     else:
         ordors.append(Ordor())
+    if not ordors[-1].checkordor():
+        ordors.pop(-1)
     done=False
     while not done:
-        print("1.View ordors\n2.Edit Ordor\n3.Place new ordor")
+        print("1.View ordors\n2.Edit Ordor\n3.Place new ordor\n4.Remove and ordor\n5.leave")
         inp=input("")
         if inp=="1":
             print(Ordor.viewordorlist(ordors))
@@ -138,4 +144,12 @@ while inp=="yes":
             ordors[inp-1].changeitem(input(f"What item would you like to change?: \nDrink, Appetizer, Main Course, Side, Second Side, Dessert\n").lower().capitalize())
         elif inp=="3":
             done=True
+            inp="yes"
+        elif inp=="4":
+            print(Ordor.viewordorlist(ordors))
+            inp=int(input("Wich ordor would you like to remove: \n"))
+            ordors.pop(inp-1)
+        elif inp=="5":
+            done=True
+    
 print("ok bye")
