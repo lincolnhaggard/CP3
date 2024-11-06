24a81cea264a973ce72b6b8ca3cd706104c30d9d
lincolnhaggard <lincoln.haggard@ucas-edu.net>
2024-11-06 16:52:11 +0000

    dog classes

---
+++ dog class practice.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+
+dogs=[
+    Dog("Marceline","German Shepherd"),
+2	Cajun	Belgian Malinois
+3	Daisy	Border Collie
+4	Rocky	Golden Retriever
+5	Bella	Irish Setter)
+
+]
