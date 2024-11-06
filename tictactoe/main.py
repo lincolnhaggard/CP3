5a971c075e581295553782293fb9b992d5727a87
lincolnhaggard <lincoln.haggard@ucas-edu.net>
2024-11-06 18:49:04 +0000

    more tic tac toe

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -8,10 +8,11 @@ class Board:
                     ["N","N","N"]]
     
     def __str__(self):
-        toret="=======\n"
-        for i in self.board:
+        toret=" 1 2 3\n"
+        for x,i in enumerate(self.board):
+            toret+=str(x)
             for k in i:
-                toret+=""
+                toret+="|"
                 if k=="N":
                     toret+=" "
                 else:
@@ -20,6 +21,22 @@ class Board:
             toret+="|\n"
 
         return toret
+    
+    def checkwin(self):
+        for win in ("X","O"):
+            for i in range(3):
+                if self.board[i][0]==win and self.board[i][1]==win and self.board[i][2]==win:
+                    return win
+                if self.board[0][i]==win and self.board[1][i]==win and self.board[2][i]==win:
+                    return win
+            if self.board[0][0]==win and self.board[1][1]==win and self.board[2][2]==win:
+                return win
+            if self.board[2][0]==win and self.board[1][1]==win and self.board[0][2]==win:
+                return win
+        return "N"
+    
+    def makemove(self,turn,pos1,pos2):
+        pass
     
 board=Board()
 print(board)
